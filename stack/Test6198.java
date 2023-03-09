88e093f31029bee468f7e8fd4d284322694189f7
kimjoenghyeon123 <hello.git.gojhkim123456@gmail.com>
2023-03-09 17:50:53 +0900

    스택 6198번 오류있음

--- stack/Test6198.java
+++ stack/Test6198.java
@@ -4,13 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
-class Top {
+class Building {
 	int num;
 	int height;
 	
-	Top(int num, int height){
+	Building(int num, int height){
 		this.num    = num;
 		this.height = height;
 	}
@@ -20,36 +19,35 @@ public class Test6198 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Stack<Top> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i <= N; i++) {
-			int height = Integer.parseInt(st.nextToken());
+		Stack<Building> stack = new Stack<>();
+		int count = 0;
+		
+		for(int i=1;i<=N;i++) {
+			Building building = new Building(i, Integer.parseInt(br.readLine()));
 			
 			if(stack.isEmpty()) {
-				stack.push(new Top(i, height));
-				sb.append("0 ");
+				stack.push(building);
 			}
 			else {
-				while(true) {
-					if(stack.isEmpty()) {
-						stack.push(new Top(i, height));
-						sb.append("0 ");
-						break;
-					}
-					
-					Top top = stack.peek(); 
-					if(top.height > height) {
-						stack.push(new Top(i, height));
-						sb.append(top.num + " ");
-						break;
-					}
-					else {
-						stack.pop();
-					}
+				while(stack.peek().height <= building.height) {
+					stack.pop();
+					if(stack.isEmpty()) break;
+				}
+				if(stack.isEmpty() || stack.peek().height > building.height) {
+					stack.push(building);
+					count += stack.size() - 1;
 				}
 			}
 		}
-		System.out.println(sb);
+		System.out.println(count);
 	}
 }
+
+/*
+ * 10
+ * 10 3  -->  count = 1
+ * 10 7  -->  count = 1 + 1 = 2
+ * 10 7 4  --> count = 2 + 2 = 4
+ * 12	   --> count = 4
+ * 12 2    --> count = 4 + 1 = 5;
+ */ 
