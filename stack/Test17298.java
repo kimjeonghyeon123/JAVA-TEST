7f687361c86936fc82537e6caeaad181b2a862c2
kimjeonghyeon123 <gojhkim123456@gmail.com>
2023-03-10 11:09:41 +0900

    스택 오큰수 성공

--- stack/Test17298.java
+++ stack/Test17298.java
@@ -1,31 +1,55 @@
 package baekjoon.stack;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
+import java.util.StringTokenizer;
+
+class NGE {
+	int sequence;
+	int num;
+	
+	NGE(int sequence, int num){
+		this.sequence = sequence;
+		this.num      = num;
+	}
+}
 
 public class Test17298 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Stack<NGE> stack = new Stack<>();
 		
-		String[] sarr = br.readLine().split(" ");
 		int[] arr = new int[N];
-		Stack<Integer> s = new Stack<>();
-		
 		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(sarr[i]);
+			arr[i] = -1;
 		}
 		
-		for(int i=0;i<N;i++) {
-			if(s.peek() > arr[i])
-				s.push(arr[i]);
-			while(s.peek() < arr[i]) {
-				s.pop();
-				s.push(arr[i]);
+		for(int i=1;i<=N;i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			if(stack.isEmpty()) {
+				stack.push(new NGE(i, temp));
+			}
+			else {
+				while(stack.peek().num < temp) {
+					arr[stack.pop().sequence - 1] = temp;
+					if(stack.isEmpty()) break;
+				}
+				stack.push(new NGE(i, temp));
 			}
 		}
+		
+		for(int i=0;i<N;i++) {
+			bw.write(arr[i] + " ");
+		}
+		
+		bw.flush();
 	}
 }
